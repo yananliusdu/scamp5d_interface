cb046d9447d79125a2676a1cbc025a1e63eb4de0
Yanan Liu <lyn.sdu@gmail.com>
2024-01-03 10:27:26 +0800

    Update AutoFocus.py

--- scamp_python_module/AutoFocus.py
+++ scamp_python_module/AutoFocus.py
@@ -25,8 +25,15 @@ target = 0
 total_pixel = 0
 
 max_pixel_num = 0
+min_pixel_num = 900
 focus_cmd = 0
 lens_cmd = 0
+
+time_delay_lens = 0.05
+lens_step = 50
+lens_cmd_start = 300
+focus_scale_min = 0.9
+focus_scale_max = 1.2
 
 # Open serial port with specified settings
 ser = serial.Serial('COM4', 9600, timeout=1, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
@@ -34,15 +41,25 @@ ser = serial.Serial('COM4', 9600, timeout=1, parity=serial.PARITY_NONE, stopbits
 # Hexadecimal commands for different levels
 commands = {
     0: bytearray([0x53, 0x31, 0x01, 0x02, 0x00, 0x00, 0x87, 0x3E]),
+    50: bytearray([0x53, 0x31, 0x01, 0x02, 0x32, 0x00, 0xB9, 0x3E]),
     100: bytearray([0x53, 0x31, 0x01, 0x02, 0x64, 0x00, 0xEB, 0x3E]),
+    150: bytearray([0x53, 0x31, 0x01, 0x02, 0x96, 0x00, 0x1D, 0x3E]),
     200: bytearray([0x53, 0x31, 0x01, 0x02, 0xC8, 0x00, 0x4F, 0x3E]),
+    250: bytearray([0x53, 0x31, 0x01, 0x02, 0xFA, 0x00, 0x81, 0x3E]),
     300: bytearray([0x53, 0x31, 0x01, 0x02, 0x2C, 0x01, 0xB4, 0x3E]),
+    350: bytearray([0x53, 0x31, 0x01, 0x02, 0x5E, 0x01, 0xE6, 0x3E]),
     400: bytearray([0x53, 0x31, 0x01, 0x02, 0x90, 0x01, 0x18, 0x3E]),
+    450: bytearray([0x53, 0x31, 0x01, 0x02, 0xC2, 0x01, 0x4A, 0x3E]),
     500: bytearray([0x53, 0x31, 0x01, 0x02, 0xF4, 0x01, 0x7C, 0x3E]),
+    550: bytearray([0x53, 0x31, 0x01, 0x02, 0x26, 0x02, 0xAF, 0x3E]),
     600: bytearray([0x53, 0x31, 0x01, 0x02, 0x58, 0x02, 0xE1, 0x3E]),
+    650: bytearray([0x53, 0x31, 0x01, 0x02, 0x8A, 0x02, 0x13, 0x3E]),
     700: bytearray([0x53, 0x31, 0x01, 0x02, 0xBC, 0x02, 0x45, 0x3E]),
+    750: bytearray([0x53, 0x31, 0x01, 0x02, 0xEE, 0x02, 0x77, 0x3E]),
     800: bytearray([0x53, 0x31, 0x01, 0x02, 0x20, 0x03, 0xAA, 0x3E]),
+    850: bytearray([0x53, 0x31, 0x01, 0x02, 0x52, 0x03, 0xDC, 0x3E]),
     900: bytearray([0x53, 0x31, 0x01, 0x02, 0x84, 0x03, 0x0E, 0x3E]),
+    950: bytearray([0x53, 0x31, 0x01, 0x02, 0xB6, 0x03, 0x40, 0x3E]),
     1000: bytearray([0x53, 0x31, 0x01, 0x02, 0xE8, 0x03, 0x72, 0x3E]),
 }
 
@@ -78,7 +95,7 @@ def run_for_duration(duration):
         # if packet is not None:
         #     loopcounter = packet.get('loopcounter', 'default_value')
         #     print('loopcounter test', loopcounter)
-    time.sleep(0.01) # in case the buffer is empty
+    time.sleep(0.02) # in case the buffer is empty
 
 def process_packet(packet):
     global DisplayCanvas
@@ -114,7 +131,7 @@ def process_packet(packet):
         elif datatype == 'SCAMP5_AOUT':
             w = packet['width']
             h = packet['height']
-            print('[%d] aout %dx%d >> %d' % (lc, w, h, packet['channel']))
+            # print('[%d] aout %dx%d >> %d' % (lc, w, h, packet['channel']))
             img = Image.frombytes('L', (w, h), packet['buffer']).transpose(Image.FLIP_LEFT_RIGHT).transpose(
                 Image.ROTATE_180)
             DisplayImage[record_image_num] = ImageTk.PhotoImage(img)
@@ -124,7 +141,7 @@ def process_packet(packet):
         elif datatype == 'SCAMP5_DOUT':
             w = packet['width']
             h = packet['height']
-            print('[%d] dout %dx%d >> %d' % (lc, w, h, packet['channel']))
+            # print('[%d] dout %dx%d >> %d' % (lc, w, h, packet['channel']))
             img = Image.frombytes('L', (w, h), packet['buffer']).transpose(Image.FLIP_LEFT_RIGHT).transpose(
                 Image.ROTATE_180)
             DisplayImage[record_image_num] = ImageTk.PhotoImage(img)
@@ -174,10 +191,10 @@ def auto_focus_process():
     # 初始对焦过程
     max_pixel_num = 0
     focus_cmd = 0
-    lens_cmd = 0
+    lens_cmd = lens_cmd_start
     while lens_cmd < 1001:
         send_command(lens_cmd)
-        run_for_duration(0.04)
+        run_for_duration(time_delay_lens)
         packet = scamp.get_packet()
         if packet is not None:
             datatype = packet['datatype']
@@ -186,7 +203,9 @@ def auto_focus_process():
                 if total_pixel > max_pixel_num*1.1:
                     max_pixel_num = total_pixel
                     focus_cmd = lens_cmd
-                lens_cmd += 100
+            else:
+                process_packet(packet)
+            lens_cmd += lens_step
 
     # 设置到最佳焦距
     send_command(focus_cmd)
@@ -194,31 +213,33 @@ def auto_focus_process():
 
     # 持续监测对焦
     while True:
-        run_for_duration(0.04)
+        run_for_duration(time_delay_lens)
         packet = scamp.get_packet()
         if packet is not None:
             # datatype = packet['datatype']
             # if datatype == 'TEXT':
             process_packet(packet)
-            if total_pixel < max_pixel_num * 0.9:  # 如果像素数量下降超过10%，重新对焦
+            if (total_pixel < min_pixel_num) or(total_pixel < max_pixel_num * focus_scale_min) or (total_pixel > max_pixel_num * focus_scale_max):  # 如果像素数量下降超过10%，重新对焦
                 max_pixel_num = 0
-                for lens_cmd in range(0, 1001, 100):
+                for lens_cmd in range(lens_cmd_start, 1001, lens_step):
                     send_command(lens_cmd)
-                    run_for_duration(0.04)
+                    run_for_duration(time_delay_lens)
                     packet = scamp.get_packet()
                     if packet is not None:
                         datatype = packet['datatype']
                         if datatype == 'TEXT':
                             process_packet(packet)
-                            if total_pixel > max_pixel_num:
+                            if total_pixel > max_pixel_num*1.1:
                                 max_pixel_num = total_pixel
                                 focus_cmd = lens_cmd
+                        else:
+                            process_packet(packet)
                 send_command(focus_cmd)
 
 # UI update
 def main_process():
     tk_root.update_idletasks()
-    tk_root.after(1, main_process)
+    tk_root.after(0.1, main_process)
 
 def api_main_process():
     scamp.routine()
